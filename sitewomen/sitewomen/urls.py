dd4abcf8552e5f241c406570dcc634bc96968611
BylatZM <ghost.stop@yandex.ru>
2024-12-21 18:49:32 +0300

    created women.urls, created urlpatterns in women.urls, added two path for urlpatterns in women.urls, modified sitewomen.urls, added include(women.urls) path and writed help comment, added simple func views in women.views

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from women import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+'''
+Прописывая url адресов в основном приложении не правильно, так как нарушается приницип автономности приложений
+(если поменяются url адреса в одном приложении, то в этом надо будет руками изменения править, а это плохо)
+
+Так не правильно !!!
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index), # localhost:8000
+    path('cats/', views.categories), # localhost:8000/cats/
+]
+Так правильно
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('women.urls')),
+]
+'''
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,1 +1,7 @@
+from django.http import HttpResponse
 
+def index(request): # request экземпляр класса HttpRequest
+  return HttpResponse("Страница приложения women.")
+
+def categories(request):
+  return HttpResponse("<h1>Статьи по категориями</h1>")
