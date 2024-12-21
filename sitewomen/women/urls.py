5651330234822c507007596b8e1d31b6cb3ead54
BylatZM <ghost.stop@yandex.ru>
2024-12-21 20:32:09 +0300

    created women.converters, added FourDigitYearConverter class in the file, added categories_by_slug and archive functions in women.views file, added cats/<slug:cat_slug>, archive/(?P<year>[0-9]{4})/, another/archive/<year4:year>/, registered own converter, writed usefull comment in women.urls

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,7 +1,36 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path, register_converter
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, "year4") # функция регистрации собственных конвертеров
 
 urlpatterns = [
   path('', views.index), # localhost:8000
-  path('cats/', views.categories) # localhost:8000/cats/
+  path('cats/<int:cat_id>/', views.categories), # пример возможного url: localhost:8000/cats/2/
+  path('cats/<slug:cat_slug>/', views.categories_by_slug), # пример возможного url: localhost:8000/cats/cdcsdcsd/
+  # позволяет прописать url используя регулярное выражение
+  re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive), # пример возможного url: localhost:8000/archive/2002
+  # объявление url с собственным конвертером
+  path('another/archive/<year4:year>/', views.archive), # пример возможного url: localhost:8000/archive/2002
 ]
+
+'''
+При запросе django проходиться по всем path и смотрит, где совпадает url адрес, после нахождения совпадения отрабатывает определенная
+функция view
+
+path('cats/<int:cat_id>/', ...) - такому path в url должно передаваться только число, которое будет присвоено переменной cat_id
+cat_id можно будет получить в функции views.categories
+
+виды конвертаров в url:
+* str - принимает строку, КРОМЕ символа "/"
+* int - принимает числовые значения
+* slug - набор английских букв, цифр, подчеркиваний, дефисов, КРОМЕ символа "/"
+* uuid - принимает цифры, малые английские буквы и дефис
+* path - любая не пустая строка, включая символ "/"
+
+Конвертер slug включается в себя конвертер int, поэтому, если url будут совпадать, и при этом url где принимается конвертер
+slug будет стоять выше, то path с url включающий конвертер int никогда не сработает
+
+Второй path не сработает, так как будет всегда отрабатывать первый !!
+path('cats/<slug:cat_slug>/', views.categories_by_slug)
+path('cats/<int:cat_id>/', views.categories)
+'''
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 def index(request): # request экземпляр класса HttpRequest
   return HttpResponse("Страница приложения women.")
 
-def categories(request):
-  return HttpResponse("<h1>Статьи по категориями</h1>")
+def categories(request, cat_id): # cat_id параметр, который передается через url для маршрута localhost:8000/cats/<число>/
+  return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+  return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+def archive(request, year):
+  return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
