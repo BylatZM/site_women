3a1d4d81148b157d47031078a3bc1cc680d23c14
BylatZM <ghost.stop@yandex.ru>
2024-12-22 09:55:35 +0300

    created index.html in women/templates/women folder, updated index function, added render_to_string function in women.views

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponseNotFound
 from datetime import datetime
 from django.shortcuts import redirect, reverse
+from django.template.loader import render_to_string # функция преобразует html шаблоны в строку, чтобы их можно было передавать в соответствующие классы
 
 def index(request): # request экземпляр класса HttpRequest
-  return HttpResponse("Страница приложения women.")
+  t = render_to_string('women/index.html') # преобразует html в строку
+  return HttpResponse(t) 
 
 def categories(request, cat_id): # cat_id параметр, который передается через url для маршрута localhost:8000/cats/<число>/
   return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
@@ -22,38 +24,9 @@ def categories_by_slug(request, cat_slug):
 def archive(request, year):
   if year > datetime.now().year:
     uri = reverse('cats', args=('music', )) # функция reverse формирует url адрес, позволяет удобно передавать аргументы через список, кортеж
-    return HttpResponseRedirect(uri) # альтернатива redirect, делает перенаправление с кодом 302, принимает либо строку url либо результат функции reverse
-    #return HttpResponsePermanentRedirect(uri) # альтернатива redirect, делает перенаправление с кодом 301, принимает либо строку url либо результат функции reverse
+    return redirect(uri)
   return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 # функция для обработки ошибки 404
 def page_not_found(request, exception):
   return HttpResponseNotFound("<h1>Страница не найдена</h1>")
-
-'''
-виды кодов редиректов:
-* 301 - страница перемещена на другой постоянный URL-адрес
-* 302 - страница перемещена временно на другой URL-адрес
-
-Функция redirect в качестве первого параметра может принимать:
-* redirect('/') - строка url адрес
-* redirect(index) - функцию представления
-* redirect('home') - имя маршрута (рекоммендуемый вариант)
-
-Функция redirect может принимать дополнительные параметры, как аргументы для url адреса
-* redirect('cats', 'music') - где 'music' является url аргументом конвертера slug
-
-Перед функцией redirect обязательно нужно указать return
-
-Чтобы сделать редирект с кодом 302, можно использовать:
-------------------
-from django.shortcuts import redirect
-return redirect('/')
-------------------
-
-Чтобы сделать редирект с кодом 301, можно использовать:
-------------------
-from django.shortcuts import redirect
-redirect('/', permanent=True)
-------------------
-'''
